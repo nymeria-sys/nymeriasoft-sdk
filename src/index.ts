9bc3df16456ad951d3b796ae3b208de171f3e53d
leonetosoft <leonardoaquinoneto@gmail.com>
2024-09-04 20:10:35 -0400

    feat: prepare for npm publication

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,4 @@
-import { NsCredentials } from "./nymeria.soft.sdk";
-
-const nsCredentials = new NsCredentials({
-  instance_uri: process.env.INSTANCE_URI || "https://",
-  client_id: process.env.CLIENT_ID || "test",
-  client_secret: process.env.CLIENT_SECRET || "test",
-  redirect_uri: process.env.REDIRECT_URI || "https://",
-  auth_type: "BROWSER",
-});
+export * from "./nymeria.soft.sdk";
+export * from "./cred.file.store";
+export * from "./realtime";
+export * from "./interface/token.interface";
