d255fb7d57c8b2c9dc3d03de532b5ca4167d8619
leonetosoft <leonardoaquinoneto@gmail.com>
2024-09-19 02:10:53 -0400

    fix: WebSocket was closed before the connection was established

--- src/realtime.ts
+++ src/realtime.ts
@@ -1,6 +1,31 @@
 import NymeriaSoftSDK from "./nymeria.soft.sdk";
 import WebSocket, { ClientOptions } from "ws";
 import ReconnectingWebSocket, { Options } from "reconnecting-websocket";
+import { type UrlProvider } from "reconnecting-websocket";
+const wsFactory = (options: ClientOptions) =>
+  class WsOptions extends WebSocket {
+    constructor(url: string, protocols?: string | string[]) {
+      super(url, protocols, options);
+    }
+  };
+
+export class KeepAliveWs extends ReconnectingWebSocket {
+  constructor(
+    url: UrlProvider,
+    protocols?: string | string[],
+    options: Options = {}
+  ) {
+    const { connectionTimeout = 4000 } = options;
+    const opts = {
+      ...options,
+      connectionTimeout: connectionTimeout + 1000, // give WS time to disconnect without throwing
+      WebSocket: wsFactory({ handshakeTimeout: connectionTimeout }), // PASS CUSTOM ARGUMENTS HERE
+    };
+
+    super(url, protocols, opts);
+  }
+}
+
 export default function (
   sdk: NymeriaSoftSDK,
   reconnect_opts?: Partial<Options>,
@@ -22,13 +47,11 @@ export default function (
   }
 
   const options = {
-    WebSocket: WebSocket, // custom WebSocket constructor
-    connectionTimeout: 20000,
     maxRetries: 10000,
     ...reconnect_opts,
   };
 
-  const rws = new ReconnectingWebSocket(
+  const rws = new KeepAliveWs(
     sdk.getConfig().realtime_uri!,
     {
       headers: {
